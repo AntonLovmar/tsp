d92956021d05ab9504d80d6fdbe00e877c7dfd9f
AntonLovmar <antonlovmar@gmail.com>
2013-11-28 16:14:37 +0100

    Added code base for implementations of Graph and Vertex. Rename them

---
+++ src/tsp/graph/TSPGraph.java
@@ -0,0 +1,29 @@
+package tsp.graph;
+
+import java.util.List;
+
+public class TSPGraph implements Graph {
+
+	public TSPGraph(List<Vertex> vertices, List<Edge> edges) {
+		
+	}
+	
+	@Override
+	public List<Edge> getEdges() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vertex> getVertices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Edge> getEdgesForVertex(Vertex vertex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/tsp/graph/VertexImpl.java
@@ -0,0 +1,28 @@
+package tsp.graph;
+
+public class VertexImpl implements Vertex {
+	
+	
+	public VertexImpl(int id, int x, int y) {
+		
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getXCoordinate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getYCoordinate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
