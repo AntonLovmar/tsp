3131b189cde920d2b723e4c33768759cbb41b2b2
AntonLovmar <antonlovmar@gmail.com>
2013-12-01 14:28:12 +0100

    Finished initial tests for InputReader. It still needs to add edges

--- src/tsp/parser/InputReader.java
+++ src/tsp/parser/InputReader.java
@@ -1,8 +1,10 @@
 package tsp.parser;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +18,13 @@ public class InputReader {
 	private BufferedReader reader;
 	
 	public InputReader() {
-		reader = new BufferedReader(new InputStreamReader(System.in));
+		//reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			FileReader fileReader = new FileReader(new File("/home/anton/Github/tsp/tests/parsetest.txt"));
+			reader = new BufferedReader(fileReader);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Graph readInput() {
@@ -27,14 +35,17 @@ public class InputReader {
 			for(int i = 0; i < numberOfVertices; i++) {
 				line = reader.readLine();
 				String[] xAndY = line.split(" ");
-				int x = Integer.parseInt(xAndY[0]);
-				int y = Integer.parseInt(xAndY[1]);
+				double x = Double.parseDouble(xAndY[0]);
+				double y = Double.parseDouble(xAndY[1]);
 				vertices.add(new VertexImpl(i, x, y));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		List<Edge> edges = new ArrayList<Edge>();
+		for(Vertex vertex : vertices) {
+			
+		}
 		Graph graph = new TSPGraph(vertices, edges);
 		return graph;
 	}
--- src/tsp/tests/InputReaderTest.java
+++ src/tsp/tests/InputReaderTest.java
@@ -1,6 +1,6 @@
 package tsp.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
