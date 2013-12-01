68fabcc1b85909e4f1d1cdc538be2b890894d759
AntonLovmar <antonlovmar@gmail.com>
2013-12-01 19:22:10 +0100

    InputReader is now implemented and working

--- src/tsp/tests/InputReaderTest.java
+++ src/tsp/tests/InputReaderTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import tsp.graph.Graph;
-import tsp.parser.InputReader;
+import tsp.reader.InputReader;
 
 public class InputReaderTest {
 
