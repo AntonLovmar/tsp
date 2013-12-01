8a021af57d45573136f10e6af286c884bdbcbcfa
AntonLovmar <antonlovmar@gmail.com>
2013-12-01 14:48:36 +0100

    Finished implementation of edge

--- src/tsp/tests/EdgeImplTest.java
+++ src/tsp/tests/EdgeImplTest.java
@@ -25,8 +25,48 @@ public class EdgeImplTest {
 		Vertex vertex2 = Mockito.mock(Vertex.class);
 		Mockito.when(vertex2.getX()).thenReturn(6.77);
 		Mockito.when(vertex2.getY()).thenReturn(5.56);
-		Edge edge = new EdgeImpl(vertex1, vertex2);
-		assertEquals(13, edge.length());
 		
+		Edge edge = new EdgeImpl(vertex1, vertex2);
+		assertEquals(13, edge.length());	
+	}
+	
+	@Test
+	public void updateExistingVertexWithNew() {
+		Vertex vertex1 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex1.getId()).thenReturn(1);
+		Mockito.when(vertex1.getX()).thenReturn(2.32);
+		Mockito.when(vertex1.getY()).thenReturn(3.67);
+		Vertex vertex2 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex2.getId()).thenReturn(2);
+		Mockito.when(vertex2.getX()).thenReturn(6.77);
+		Mockito.when(vertex2.getY()).thenReturn(5.56);
+		
+		Vertex vertex3 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex1.getId()).thenReturn(3);
+		Mockito.when(vertex2.getX()).thenReturn(6.77);
+		Mockito.when(vertex2.getY()).thenReturn(5.56);
+		
+		Edge edge = new EdgeImpl(vertex1, vertex2);
+		assertTrue(edge.updateEdge(vertex1, vertex3));
+	}
+	
+	@Test
+	public void updateNonExistingVertex() {
+		Vertex vertex1 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex1.getId()).thenReturn(1);
+		Mockito.when(vertex1.getX()).thenReturn(2.32);
+		Mockito.when(vertex1.getY()).thenReturn(3.67);
+		Vertex vertex2 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex2.getId()).thenReturn(2);
+		Mockito.when(vertex2.getX()).thenReturn(6.77);
+		Mockito.when(vertex2.getY()).thenReturn(5.56);
+		
+		Vertex vertex3 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex1.getId()).thenReturn(3);
+		Mockito.when(vertex2.getX()).thenReturn(6.77);
+		Mockito.when(vertex2.getY()).thenReturn(5.56);
+		
+		Edge edge = new EdgeImpl(vertex1, vertex2);
+		assertFalse(edge.updateEdge(vertex3, vertex3));
 	}
 }
