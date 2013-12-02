b240bc9899ae027e0e05e877ad89f07971958d05
AntonLovmar <antonlovmar@gmail.com>
2013-12-02 11:18:52 +0100

    Added code base for a naive path finding strategy

---
+++ src/tsp/pathfinding/NaiveStrategy.java
@@ -0,0 +1,25 @@
+package tsp.pathfinding;
+
+import tsp.graph.Graph;
+import tsp.graph.Path;
+
+public class NaiveStrategy implements PathfindingStrategy {
+
+	/*GreedyTour
+   tour[0] = 0
+   used[0] = true
+   for i = 1 to n-1
+      best = -1
+      for j = 0 to n-1
+         if not used[j] and (best = -1 or dist(tour[i-1],j) < dist(tour[i-1],best))
+	    best = j
+      tour[i] = best
+      used[best] = true
+   return tour
+   */
+	@Override
+	public Path findPath(Graph graph) {
+		return null;
+	}
+
+}
