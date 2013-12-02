9385b8384362ec487a2a330866baa5c3cbbc860a
AntonLovmar <antonlovmar@gmail.com>
2013-12-02 11:17:53 +0100

    Implemented Path with unit testing

---
+++ src/tsp/graph/PathImpl.java
@@ -0,0 +1,50 @@
+package tsp.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathImpl implements Path {
+	
+	private List<Vertex> path;
+	
+	public PathImpl() {
+		path = new ArrayList<Vertex>();
+	}
+
+	@Override
+	public List<Vertex> getPath() {
+		return path;
+	}
+
+	@Override
+	public void addToPath(Vertex vertex) {
+		path.add(vertex);
+	}
+
+	@Override
+	public void replaceVertex(int index, Vertex newVertex) {
+		path.remove(index);
+		path.add(index, newVertex);
+	}
+
+	@Override
+	public void swapVerticesAt(int firstIndex, int secondIndex) {
+		Vertex first = path.get(firstIndex);
+		Vertex second = path.get(secondIndex);
+		path.remove(firstIndex);
+		path.add(firstIndex, second);
+		path.remove(secondIndex);
+		path.add(secondIndex, first);
+	}
+
+	@Override
+	public Vertex getVertex(int index) {
+		return path.get(index);
+	}
+
+	@Override
+	public Vertex getFirst() {
+		return path.get(0);
+	}
+
+}
---
+++ src/tsp/tests/PathImplTest.java
@@ -0,0 +1,58 @@
+package tsp.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import tsp.graph.Path;
+import tsp.graph.PathImpl;
+import tsp.graph.Vertex;
+
+public class PathImplTest {
+
+	@Test
+	public void replaceVertex() {
+		Vertex vertex = Mockito.mock(Vertex.class);
+		Mockito.when(vertex.getId()).thenReturn(1);
+		Vertex vertex2 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex2.getId()).thenReturn(2);
+		Vertex vertex3 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex3.getId()).thenReturn(3);
+		Vertex vertex4 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex4.getId()).thenReturn(4);
+		
+		Path path = new PathImpl();
+		path.addToPath(vertex);
+		path.addToPath(vertex2);
+		path.addToPath(vertex3);
+		
+		path.replaceVertex(2, vertex4);
+		assertEquals(4, path.getVertex(2).getId());
+	}
+	
+	@Test
+	public void swapVertices() {
+		Vertex vertex = Mockito.mock(Vertex.class);
+		Mockito.when(vertex.getId()).thenReturn(1);
+		Vertex vertex2 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex2.getId()).thenReturn(2);
+		Vertex vertex3 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex3.getId()).thenReturn(3);
+		Vertex vertex4 = Mockito.mock(Vertex.class);
+		Mockito.when(vertex4.getId()).thenReturn(4);
+		
+		Path path = new PathImpl();
+		path.addToPath(vertex);
+		path.addToPath(vertex2);
+		path.addToPath(vertex3);
+		path.addToPath(vertex4);
+		
+		path.swapVerticesAt(1, 3);
+		assertEquals(4, path.getVertex(1).getId());
+		assertEquals(2, path.getVertex(3).getId());
+		assertEquals(1, path.getVertex(0).getId());
+		assertEquals(3, path.getVertex(2).getId());
+	}
+
+}
