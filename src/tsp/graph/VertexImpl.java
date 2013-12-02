13b031771948e14142a67e3ab0a3bfb42bcd8ac4
AntonLovmar <antonlovmar@gmail.com>
2013-12-02 11:17:06 +0100

    Removed space in VertexImpl... >_<

--- src/tsp/graph/VertexImpl.java
+++ src/tsp/graph/VertexImpl.java
@@ -1,10 +1,10 @@
 package tsp.graph;
 
 public class VertexImpl implements Vertex {
-	
-	private double x,y;
+
+	private double x, y;
 	private int id;
-	
+
 	public VertexImpl(int id, double x, double y) {
 		this.x = x;
 		this.y = y;
@@ -25,12 +25,13 @@ public class VertexImpl implements Vertex {
 	public double getY() {
 		return y;
 	}
-	
+
 	@Override
 	public boolean equals(Object other) {
 		Vertex otherVertex = (Vertex) other;
-		if(this.getId() == otherVertex.getId()) {
-			return true; 
+		if (this.getId() == otherVertex.getId() && this.getX() == otherVertex.getX()
+				&& this.getY() == otherVertex.getY()) {
+			return true;
 		} else {
 			return false;
 		}
