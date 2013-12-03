c0281ad96cdd8df08bb78ec20ee55368723be5fc
Peter Nycander <peter.nycander@gmail.com>
2013-12-03 12:56:36 +0100

    Fixed distance test

--- tests/tsp/graph/TSPGraphTest.java
+++ tests/tsp/graph/TSPGraphTest.java
@@ -14,11 +14,18 @@ public class TSPGraphTest {
 	public void getDistanceBetweenVertices() {
 		Vertex vertex1 = Mockito.mock(Vertex.class);
 		Mockito.when(vertex1.getId()).thenReturn(1);
+		Mockito.when(vertex1.getX()).thenReturn(0.0);
+		Mockito.when(vertex1.getY()).thenReturn(0.0);
 
 		Vertex vertex2 = Mockito.mock(Vertex.class);
 		Mockito.when(vertex2.getId()).thenReturn(2);
+		Mockito.when(vertex2.getX()).thenReturn(1.0);
+		Mockito.when(vertex2.getY()).thenReturn(1.0);
+
 		Vertex vertex3 = Mockito.mock(Vertex.class);
 		Mockito.when(vertex3.getId()).thenReturn(3);
+		Mockito.when(vertex3.getX()).thenReturn(3.0);
+		Mockito.when(vertex3.getY()).thenReturn(4.0);
 
 		List<Vertex> vertices = new ArrayList<Vertex>();
 		vertices.add(vertex1);
@@ -26,9 +33,9 @@ public class TSPGraphTest {
 		vertices.add(vertex3);
 
 		Graph graph = new TSPGraph(vertices);
-		assertEquals(3, graph.distanceBetween(vertex1, vertex2));
-		assertEquals(6, graph.distanceBetween(vertex1, vertex3));
-		assertEquals(8, graph.distanceBetween(vertex2, vertex3));
+		assertEquals(1, graph.distanceBetween(vertex1, vertex2));
+		assertEquals(5, graph.distanceBetween(vertex1, vertex3));
+		assertEquals(4, graph.distanceBetween(vertex2, vertex3));
 
 	}
 }
